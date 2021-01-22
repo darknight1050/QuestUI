323d1f4aad376c997e4c4a406dc149c614f69025
darknight1050 <darkdeveloping1050@gmail.com>
2021-01-22 11:09:53 +0100

    Version Bump libs

--- Android.mk
+++ Android.mk
@@ -19,23 +19,23 @@ TARGET_ARCH_ABI := $(APP_ABI)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hook
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-# Creating prebuilt for dependency: codegen - version: 0.6.0
+# Creating prebuilt for dependency: codegen - version: 0.6.1
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_6_0
+LOCAL_MODULE := codegen_0_6_1
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_6_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_6_1.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.4.2
+# Creating prebuilt for dependency: custom-types - version: 0.4.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.9
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.10
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_0_9
+LOCAL_MODULE := beatsaber-hook_1_0_10
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_9.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_10.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.0.4
 include $(CLEAR_VARS)
@@ -50,9 +50,9 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += codegen_0_6_0
+LOCAL_SHARED_LIBRARIES += codegen_0_6_1
 LOCAL_SHARED_LIBRARIES += custom-types
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_9
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_10
 LOCAL_LDLIBS += -llog 
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -I"extern" -I"extern/codegen/include" -DVERSION='"0.0.1"' -Wno-invalid-offsetof
 LOCAL_C_INCLUDES += ./include ./src 
