f77add84e59d11ec2588788d33f2d8d89dc37ced
darknight1050 <darkdeveloping1050@gmail.com>
2021-04-05 18:44:15 +0200

    Update to bshook 1.2.4

--- Android.mk
+++ Android.mk
@@ -30,11 +30,11 @@ LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.4
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_2_3
+LOCAL_MODULE := beatsaber-hook_1_2_4
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_4.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.0.4
 include $(CLEAR_VARS)
@@ -51,7 +51,7 @@ LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_SHARED_LIBRARIES += codegen_0_7_0
 LOCAL_SHARED_LIBRARIES += custom-types
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_3
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_4
 LOCAL_LDLIBS += -llog 
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -I"extern" -I"extern/codegen/include" -DVERSION='"0.0.1"' -Wno-invalid-offsetof
 LOCAL_C_INCLUDES += ./include ./src 
