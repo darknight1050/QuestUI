cd6bb7210267cd6f3b48db58683142981d3b8eaf
darknight1050 <darkdeveloping1050@gmail.com>
2021-04-10 23:44:38 +0200

    Update codegen

--- Android.mk
+++ Android.mk
@@ -18,11 +18,11 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: codegen - version: 0.7.0
+# Creating prebuilt for dependency: codegen - version: 0.7.1
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_7_0
+LOCAL_MODULE := codegen_0_7_1
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_7_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_7_1.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.8.2
 include $(CLEAR_VARS)
@@ -36,7 +36,7 @@ LOCAL_MODULE := beatsaber-hook_1_3_0
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
 LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_0.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: modloader - version: 1.0.4
+# Creating prebuilt for dependency: modloader - version: 1.1.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
@@ -49,7 +49,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += codegen_0_7_0
+LOCAL_SHARED_LIBRARIES += codegen_0_7_1
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_0
 LOCAL_LDLIBS += -llog 
