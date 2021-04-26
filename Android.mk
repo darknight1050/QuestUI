094183dad261ff85bfade1cddbe622dda7b180b6
darknight1050 <darkdeveloping1050@gmail.com>
2021-04-26 19:05:19 +0200

    Update custom-types

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ LOCAL_MODULE := codegen_0_7_2
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
 LOCAL_SRC_FILES := extern/libcodegen_0_7_2.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.8.2
+# Creating prebuilt for dependency: custom-types - version: 0.8.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
