0b7effb3c9ddf012485a0cfd61d67c4dd65b88a4
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-08-31 01:35:12 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -7,7 +7,8 @@ if (-not $p1)
 
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
-$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
+}
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk --output-sync=none -j $p1
