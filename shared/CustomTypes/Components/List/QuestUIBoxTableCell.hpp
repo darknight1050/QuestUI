171141c7f4d1a68764957a94533e6d516ca59024
darknight1050 <darkdeveloping1050@gmail.com>
2021-10-08 23:15:51 +0200

    Update to latest qpm libs

--- shared/CustomTypes/Components/List/QuestUIBoxTableCell.hpp
+++ shared/CustomTypes/Components/List/QuestUIBoxTableCell.hpp
@@ -10,8 +10,8 @@ DECLARE_CLASS_CODEGEN(QuestUI, QuestUIBoxTableCell, HMUI::TableCell,
     DECLARE_INSTANCE_FIELD(HMUI::ImageView*, coverImage);
     DECLARE_INSTANCE_FIELD(HMUI::ImageView*, selectionImage);
 
-    DECLARE_OVERRIDE_METHOD(void, SelectionDidChange, il2cpp_utils::il2cpp_type_check::MetadataGetter<&HMUI::TableCell::SelectionDidChange>::get(), HMUI::SelectableCell::TransitionType transitionType);
-    DECLARE_OVERRIDE_METHOD(void, HighlightDidChange, il2cpp_utils::il2cpp_type_check::MetadataGetter<&HMUI::TableCell::HighlightDidChange>::get(), HMUI::SelectableCell::TransitionType transitionType);
+    DECLARE_OVERRIDE_METHOD(void, SelectionDidChange, il2cpp_utils::il2cpp_type_check::MetadataGetter<&HMUI::SelectableCell::SelectionDidChange>::get(), HMUI::SelectableCell::TransitionType transitionType);
+    DECLARE_OVERRIDE_METHOD(void, HighlightDidChange, il2cpp_utils::il2cpp_type_check::MetadataGetter<&HMUI::SelectableCell::HighlightDidChange>::get(), HMUI::SelectableCell::TransitionType transitionType);
 
     DECLARE_INSTANCE_METHOD(void, SetComponents, HMUI::ImageView* coverImage, HMUI::ImageView* selectionImage);
     DECLARE_INSTANCE_METHOD(void, SetData, UnityEngine::Sprite* coverSprite);
