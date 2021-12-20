e9c358c9e6f9adb65f576d8110af6096e90fd0f5
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-20 10:42:35 +0100

    Convert to qpm_rust

--- copy.ps1
+++ copy.ps1
@@ -1,4 +1,19 @@
+param (
+    [Parameter(Mandatory=$false)]
+    [Switch]$debug
+    [Parameter(Mandatory=$false)]
+    [Switch]$log
+)
+
 & ./build.ps1
-& adb push libs/arm64-v8a/libquestui.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libquestui.so
+if ($debug.IsPresent) {
+    & adb push build/debug_libquestui.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libquestui.so
+} else {
+    & adb push build/libquestui.so /sdcard/Android/data/com.beatgames.beatsaber/files/libs/libquestui.so
+}
+
 & adb shell am force-stop com.beatgames.beatsaber
 & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+if ($log.IsPresent) {
+    & ./log.ps1
+}
