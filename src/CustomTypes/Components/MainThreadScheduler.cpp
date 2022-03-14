95051ebac721d14b2fdeb9c4cc8530d84b8d1125
darknight1050 <darkdeveloping1050@gmail.com>
2022-03-14 17:05:41 +0100

    Fix deadlock in MainThreadScheduler
    Run Method directly if already on main thread for MainThreadScheduler

--- src/CustomTypes/Components/MainThreadScheduler.cpp
+++ src/CustomTypes/Components/MainThreadScheduler.cpp
@@ -5,7 +5,7 @@ DEFINE_TYPE(QuestUI, MainThreadScheduler);
 
 std::vector<std::function<void()>> QuestUI::MainThreadScheduler::scheduledMethods;
 std::mutex QuestUI::MainThreadScheduler::scheduledMethodsMutex;
-
+//PersistentSingleton_1<HMMainThreadDispatcher*>::get_instance()->Enqueue(il2cpp_utils::MakeDelegate<System::Action*>(classof(System::Action*), (std::function<void()>)[=] { }));
 void QuestUI::MainThreadScheduler::Update() {
     std::lock_guard<std::mutex> lock(scheduledMethodsMutex);
     if(scheduledMethods.size() > 0) {
